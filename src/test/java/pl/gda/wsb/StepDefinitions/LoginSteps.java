3155b73ca6be8b9842a8d60a266bb434e1c6f5e7
Mateusz Sosnowski <msosnowski@wsb.gda.pl>
2023-03-12 15:21:37 +0100

    Poprawa zależności

--- src/test/java/pl/gda/wsb/StepDefinitions/LoginSteps.java
+++ src/test/java/pl/gda/wsb/StepDefinitions/LoginSteps.java
@@ -1,5 +1,9 @@
 package pl.gda.wsb.StepDefinitions;
 
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,7 +14,7 @@ public class LoginSteps {
 
     WebDriver driver = null;
 
-    @cucumber.api.java.en.Given("Open login page")
+    @Given("Open login page")
     public void openLoginPage() {
         // Declaration and instantiation of driver
         System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
@@ -21,22 +25,23 @@ public class LoginSteps {
         driver.navigate().to("https://opensource-demo.orangehrmlive.com/");
     }
 
-    @cucumber.api.java.en.When("User enters unsername and password")
+    @When("User enters unsername and password")
     public void userEntersUnsernameAndPassword() {
         System.out.println("Users enters unsername and password");
         driver.findElement(By.name("username")).sendKeys("Admin");
         driver.findElement(By.name("password")).sendKeys("admin321321");
     }
 
-    @cucumber.api.java.en.And("Click on login button")
+    @And("Click on login button")
     public void clickOnLoginButton() {
         System.out.println("Click on login button");
         driver.findElement(By.className("orangehrm-login-button")).click();
     }
 
-    @cucumber.api.java.en.Then("The validation message is displayed")
+    @Then("The validation message is displayed")
     public void theValidationMessageIsDisplayed() {
         System.out.println("Validation message");
         String validation_message = driver.findElement(By.className("oxd-alert-content-text")).getText();
+        System.out.println(validation_message);
     }
 }
--- src/test/java/pl/gda/wsb/TestRunner/TestRunner.java
+++ src/test/java/pl/gda/wsb/TestRunner/TestRunner.java
@@ -1,12 +1,13 @@
 package pl.gda.wsb.TestRunner;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "html:target/cucumber"},
+@CucumberOptions(plugin = {"pretty", "html:target/cucumber.html"},
         features = "src/test/resources/Features/",
-        glue = "pl/gda/wsb/StepDefinitions")
+        glue = "pl/gda/wsb/StepDefinitions",
+        publish = true)
 public class TestRunner {
 }
